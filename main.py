298c6764c318644f24867820a43b3de4206cc64c
Yeho <qweasdzxc12000@naver.com>
2023-11-12 17:43:00 +0900

    채ㅡㅡㅑㅅ

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
-import random
 from set import classify_images
+
+def get_image_files(folder, extensions):
+    if os.path.exists(folder):
+        files = [file for file in os.listdir(folder) if file.lower().endswith(extensions)]
+        return files
+    else:
+        return []
 
 def autonomous_vehicle_decision(left_images, right_images, auv_images):
     # 왼쪽, 오른쪽, 차안의 인원 수
@@ -23,12 +29,15 @@ input_folder = "../Trolley-dilemma/image"
 output_left_folder = "../Trolley-dilemma/left_folder"
 output_right_folder = "../Trolley-dilemma/right_folder"
 auv_folder = "../Trolley-dilemma/AUV"
-classify_images(input_folder, output_left_folder, output_right_folder,auv_folder)
+classify_images(input_folder, output_left_folder, output_right_folder, auv_folder)
+
+# 이미지 파일 확장자 목록
+image_extensions = ('.png', '.jpg', '.jpeg')
 
 # 이미지가 있는지 확인하고 없으면 빈 리스트로 설정
-left_images = os.listdir(output_left_folder) if os.path.exists(output_left_folder) else []
-right_images = os.listdir(output_right_folder) if os.path.exists(output_right_folder) else []
-auv_images = os.listdir(auv_folder) if os.path.exists(auv_folder) else []
+left_images = get_image_files(output_left_folder, image_extensions)
+right_images = get_image_files(output_right_folder, image_extensions)
+auv_images = get_image_files(auv_folder, image_extensions)
 
 # 자율주행 자동차의 판단
 decision = autonomous_vehicle_decision(left_images, right_images, auv_images)
