6de811c1a456fe0985881a63035f3baee346f519
Yeho <qweasdzxc12000@naver.com>
2023-11-13 21:38:37 +0900

    채ㅡㅡㅑㅅ

--- main.py
+++ main.py
@@ -8,24 +8,36 @@ def get_image_files(folder, extensions):
     else:
         return []
 
+def calculate_weight(images, keyword):
+    # 특정 키워드가 포함된 이미지 수
+    count = sum(keyword in image.lower() for image in images)
+    return count
+
 def autonomous_vehicle_decision(left_images, right_images, auv_images):
     # 왼쪽, 오른쪽, 차안의 인원 수
     num_left = len(left_images)
     num_right = len(right_images)
     num_auv = len(auv_images)
 
-    # 트롤리 딜레마 결정
-    if num_left > num_right and num_left > num_auv:
-        decision = "Go to right"
-    elif num_left < num_right and num_right > num_auv:
-        decision = "Go to left"
-    elif num_auv > num_left and num_auv > num_right:
-        decision = "Go to the safest route considering passengers in the vehicle"
+    # 각 상황에 대한 가중치 설정
+    weight_child = 0.5
+    weight_grand = 0.1
+    weight_patient = 0.2
+
+    # 각 상황에 대한 가중치 계산
+    weight_left = num_left + weight_child * calculate_weight(left_images, 'child') + weight_grand * calculate_weight(left_images, 'grand') + weight_patient * calculate_weight(left_images, 'patient')
+    weight_right = num_right + weight_child * calculate_weight(right_images, 'child') + weight_grand * calculate_weight(right_images, 'grand') + weight_patient * calculate_weight(right_images, 'patient')
+    weight_auv = num_auv + weight_child * calculate_weight(auv_images, 'child') + weight_grand * calculate_weight(auv_images, 'grand') + weight_patient * calculate_weight(auv_images, 'patient')
+
+    # 길 위의 사람과 차 안의 사람을 비교하여 트롤리 딜레마 결정
+    if weight_auv > max(weight_left, weight_right):
+        decision = 'Go to the safest route considering passengers in the vehicle'
+    elif weight_left > weight_right:
+        decision = 'Go to left'
     else:
-        decision = "No specific decision"
+        decision = 'Go to right'
 
-    return decision 
-
+    return decision
 
 # 예시 폴더 경로
 input_folder = "../Trolley-dilemma/image"
