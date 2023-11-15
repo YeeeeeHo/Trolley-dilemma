e2cfdabe4e98edfbf367044688b9dbc280a0ff77
Yeho <qweasdzxc12000@naver.com>
2023-11-15 17:25:54 +0900

    채ㅡㅡㅑㅅ

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def autonomous_vehicle_decision(left_images, right_images, auv_images):
 
     # 각 상황에 대한 가중치 설정
     weight_child = 0.5
-    weight_grand = 0.1
-    weight_patient = 0.2
+    weight_grand = 0.2
+    weight_patient = 0.3
 
     # 각 상황에 대한 가중치 계산
-    weight_left = num_left + weight_child * calculate_weight(left_images, 'child') + weight_grand * calculate_weight(left_images, 'grand') + weight_patient * calculate_weight(left_images, 'patient')
-    weight_right = num_right + weight_child * calculate_weight(right_images, 'child') + weight_grand * calculate_weight(right_images, 'grand') + weight_patient * calculate_weight(right_images, 'patient')
-    weight_auv = num_auv + weight_child * calculate_weight(auv_images, 'child') + weight_grand * calculate_weight(auv_images, 'grand') + weight_patient * calculate_weight(auv_images, 'patient')
+    weight_left = -(num_left + weight_child * calculate_weight(left_images, 'child') + weight_grand * calculate_weight(left_images, 'grand') + weight_patient * calculate_weight(left_images, 'patient'))
+    weight_right = -(num_right + weight_child * calculate_weight(right_images, 'child') + weight_grand * calculate_weight(right_images, 'grand') + weight_patient * calculate_weight(right_images, 'patient'))
+    weight_auv = -(num_auv + weight_child * calculate_weight(auv_images, 'child') + weight_grand * calculate_weight(auv_images, 'grand') + weight_patient * calculate_weight(auv_images, 'patient'))
 
     # 길 위의 사람과 차 안의 사람을 비교하여 트롤리 딜레마 결정
     if weight_auv > max(weight_left, weight_right):
