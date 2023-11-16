cff7d30030ae5974254023d8df626ebfc6c3d2f8
Yeho <qweasdzxc12000@naver.com>
2023-11-16 21:21:01 +0900

    채ㅡㅡㅑㅅ

--- main.py
+++ main.py
@@ -12,27 +12,33 @@ def calculate_weight(images, keywords_weights):
     # 키워드 및 가중치를 기반으로 총 가중치 계산
     total_weight = 0
     keyword_weights = {}  # 추가: 각 키워드별 가중치 저장
+
+    # 이미지 개수를 기준으로 가중치를 계산
     for keyword, weight in keywords_weights.items():
         keyword_count = sum(keyword in image.lower() for image in images)
-        total_weight += keyword_count * weight
-        keyword_weights[keyword] = keyword_count * weight
+        keyword_weight = keyword_count * weight / len(images)  # 개수로 나눠서 평균 계산
+        total_weight += keyword_weight
+        keyword_weights[keyword] = keyword_weight
+
+    # 값들을 소수점 둘째 자리까지 반올림
+    total_weight = round(total_weight, 2)
+    keyword_weights = {key: round(value, 2) for key, value in keyword_weights.items()}
+
     return total_weight, keyword_weights
 
 def autonomous_vehicle_decision(left_images, right_images, auv_images):
     # 각 상황의 인원 수
-    num_left = len(left_images)
-    num_right = len(right_images)
     num_auv = len(auv_images)
 
     # 키워드 및 가중치 정의
     keywords_weights = {
-        'child': 1.2,
+        'child': 1.3,
         'grand': 0.5,
         'patient': 0.75,
         'doctor': 1.2,
         'animal': 1,
         'police': 1.1,
-        'pregnant': 1.3,
+        'pregnant': 1.35,
         'thief': 0.1
     }
 
@@ -40,15 +46,25 @@ def autonomous_vehicle_decision(left_images, right_images, auv_images):
     weight_left, keyword_weights_left = calculate_weight(left_images, keywords_weights)
     weight_right, keyword_weights_right = calculate_weight(right_images, keywords_weights)
     weight_auv, keyword_weights_auv = calculate_weight(auv_images, keywords_weights)
-    weight_wall = num_auv + calculate_weight(auv_images, keywords_weights)[0]
 
+    # 'wall'이라는 키워드가 포함된 파일이 갈림길에서 발견되면 해당 갈림길의 가중치를 차 안의 가중치로 설정
     # 트롤리 딜레마 결정을 위해 가중치 비교
-    if 'wall' in left_images or 'wall' in right_images:
-        if weight_wall > weight_auv:
-            decision = '벽 쪽으로 이동'
+    if any('wall' in image.lower() and image.lower().endswith('.jpg') for image in left_images) or any('wall' in image.lower() and image.lower().endswith('.jpg') for image in right_images):
+    # 'wall.jpg'가 왼쪽 또는 오른쪽 폴더 안에 있는 경우
+        if any('wall' in image.lower() and image.lower().endswith('.jpg') for image in left_images):
+            weight_left = weight_auv  # 왼쪽 갈림길의 가중치를 벽 쪽 갈림길의 가중치로 설정
+        if any('wall' in image.lower() and image.lower().endswith('.jpg') for image in right_images):
+            weight_right = weight_auv  # 오른쪽 갈림길의 가중치를 벽 쪽 갈림길의 가중치로 설정
+
+        if weight_auv > weight_right:
+            decision = '오른쪽으로 이동'
         else:
-            decision = '자율주행 자동차 쪽으로 이동'
+            decision = '왼쪽으로 이동'
     else:
+        # 'wall.jpg'가 없는 경우
+        weight_left, keyword_weights_left = calculate_weight(left_images, keywords_weights)  # 왼쪽 갈림길의 가중치 계산
+        weight_right, keyword_weights_right = calculate_weight(right_images, keywords_weights)  # 오른쪽 갈림길의 가중치 계산
+
         if weight_auv > max(weight_left, weight_right):
             if weight_left < weight_right:
                 decision = '왼쪽으로 이동'
@@ -94,4 +110,5 @@ print(f"\n가중치 결과:")
 print(f"   - 왼쪽 갈림길 총 가중치: {total_weight_left}")
 print(f"   - 오른쪽 갈림길 총 가중치: {total_weight_right}")
 print(f"   - 차안 총 가중치: {total_weight_auv}")
+
 print("\n트롤리 딜레마 시뮬레이션이 완료되었습니다.")
