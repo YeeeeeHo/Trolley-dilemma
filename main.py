10eb7b12e8e4451d087faeb8f66b8cbc29c94aff
Yeho <qweasdzxc12000@naver.com>
2023-11-16 19:57:11 +0900

    commit

--- main.py
+++ main.py
@@ -8,55 +8,53 @@ def get_image_files(folder, extensions):
     else:
         return []
 
-def calculate_weight(images, keyword):
-    # 특정 키워드가 포함된 이미지 수
-    count = sum(keyword in image.lower() for image in images)
-    return count
+def calculate_weight(images, keywords_weights):
+    # 키워드 및 가중치를 기반으로 총 가중치 계산
+    total_weight = 0
+    for keyword, weight in keywords_weights.items():
+        total_weight += weight * sum(keyword in image.lower() for image in images)
+    return total_weight
 
 def autonomous_vehicle_decision(left_images, right_images, auv_images):
-    # 왼쪽, 오른쪽, 차안의 인원 수
+    # 각 상황의 인원 수
     num_left = len(left_images)
     num_right = len(right_images)
     num_auv = len(auv_images)
 
-    # 각 상황에 대한 가중치 설정
-    weight_child = 0.5
-    weight_grand = 0.2
-    weight_patient = 0.3
+    # 키워드 및 가중치 정의
+    keywords_weights = {
+        'child': 1,
+        'grand': 0.5,
+        'patient': 0.75,
+        'doctor': 1.2,
+        'animal': 0.8,
+        'police': 1.1,
+        'pregnant': 1.3,
+        'thief': 0.9
+    }
 
     # 각 상황에 대한 가중치 계산
-    weight_left = -(num_left + weight_child * calculate_weight(left_images, 'child') + 
-                    weight_grand * calculate_weight(left_images, 'grand') + 
-                    weight_patient * calculate_weight(left_images, 'patient'))
+    weight_left = -(num_left + calculate_weight(left_images, keywords_weights))
+    weight_right = -(num_right + calculate_weight(right_images, keywords_weights))
+    weight_auv = -(num_auv + calculate_weight(auv_images, keywords_weights))
+    weight_wall = num_auv + calculate_weight(auv_images, keywords_weights)
 
-    weight_right = -(num_right + weight_child * calculate_weight(right_images, 'child') + 
-                     weight_grand * calculate_weight(right_images, 'grand') + 
-                     weight_patient * calculate_weight(right_images, 'patient'))
-
-    weight_auv = -(num_auv + weight_child * calculate_weight(auv_images, 'child') + 
-                   weight_grand * calculate_weight(auv_images, 'grand') + 
-                   weight_patient * calculate_weight(auv_images, 'patient'))
-
-    weight_wall = (num_auv + weight_child * calculate_weight(auv_images, 'child') + 
-                  weight_grand * calculate_weight(auv_images, 'grand') + 
-                  weight_patient * calculate_weight(auv_images, 'patient'))
-
-    # 길 위의 사람과 차 안의 사람을 비교하여 트롤리 딜레마 결정
+    # 트롤리 딜레마 결정을 위해 가중치 비교
     if 'wall' in left_images or 'wall' in right_images:
         if weight_wall > weight_auv:
-            decision = 'Go to wall'
+            decision = '벽 쪽으로 이동'
         else:
-            decision = 'Go to autonomous vehicle'
+            decision = '자율주행 자동차 쪽으로 이동'
     else:
         if weight_auv > max(weight_left, weight_right):
             if weight_left > weight_right:
-                decision = 'Go to left'
+                decision = '왼쪽으로 이동'
             else:
-                decision = 'Go to right'
+                decision = '오른쪽으로 이동'
         elif weight_left > weight_right:
-            decision = 'Go to left'
+            decision = '왼쪽으로 이동'
         else:
-            decision = 'Go to right'
+            decision = '오른쪽으로 이동'
 
     return decision
 
